68f9fb6ea00721afe32eaf38a938a14e5ffde781
Petrenko <138616226+Naremay@users.noreply.github.com>
2023-07-09 20:35:25 +0300

    From GitHub

--- src/main/java/ua/hillel/petrenko/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/petrenko/lessons/lesson3/Main.java
@@ -3,5 +3,6 @@ package ua.hillel.petrenko.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello GitHub");
     }
 }
