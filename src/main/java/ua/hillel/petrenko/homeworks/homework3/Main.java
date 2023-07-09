41227d6e6b333cbcc1ea29a200453f655632a93e
Petrenko Alina <petrrenkoo@gmail.com>
2023-07-09 20:54:17 +0300

    For homework3 again

--- src/main/java/ua/hillel/petrenko/homeworks/homework3/Main.java
+++ src/main/java/ua/hillel/petrenko/homeworks/homework3/Main.java
@@ -2,6 +2,6 @@ package ua.hillel.petrenko.homeworks.homework3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Changes for homework");
+        System.out.println("More changes for homework");
     }
 }
