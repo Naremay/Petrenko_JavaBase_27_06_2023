d753adbbbb8134a3037123cf95c4e3955c858e3d
Petrenko Alina <petrrenkoo@gmail.com>
2023-08-08 01:54:39 +0300

    Added changes for homework13

--- src/main/java/ua/hillel/petrenko/homeworks/homework13/BurgerMain.java
+++ src/main/java/ua/hillel/petrenko/homeworks/homework13/BurgerMain.java
@@ -3,9 +3,9 @@ package ua.hillel.petrenko.homeworks.homework13;
 public class BurgerMain {
     public static void main(String[] args) {
 
-        Burger burger1 = new Burger("булочка", "м'ясо", "сир", "зелень", "майонез");
-        Burger burger2 = new Burger("булочка", "м'ясо", "сир", "зелень");
-        Burger burger3 = new Burger("булочка", "подвійне", "м'ясо", "сир", "зелень",
+        Burger standartBurger = new Burger("булочка", "м'ясо", "сир", "зелень", "майонез");
+        Burger dietBurger = new Burger("булочка", "м'ясо", "сир", "зелень");
+        Burger doubleMeatBurger = new Burger("булочка", "подвійне", "м'ясо", "сир", "зелень",
                 "майонез");
 
     }
